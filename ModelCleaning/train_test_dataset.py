0962979f1b7a95f1ff20e84ad4f385e314cea134
“rossatuai” <rossatuai@gmail.com>
2026-04-30 22:22:06 +0000

    added mlflow

--- ModelCleaning/train_test_dataset.py
+++ ModelCleaning/train_test_dataset.py
@@ -1,8 +1,14 @@
 import os
+import mlflow
 from pandas import read_csv
 from joblib import dump
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+
+
+mlflow.set_tracking_uri("http://localhost:5555")
+
+
 
 workspace = os.getenv('GITHUB_WORKSPACE')
 
@@ -30,7 +36,38 @@ y= df["Salary"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-mind = LinearRegression()
-mind.fit(X_train, y_train)
+
+with mlflow.start_run():
+
+
+    mind = LinearRegression()
+    mind.fit(X_train, y_train)
+
+
+    # Evaluate
+    predictions = mind.predict(X_test)
+    r2 = r2_score(y_test, predictions)
+    mse = mean_squared_error(y_test, predictions)
+
+    # Log Metrics
+    mlflow.log_metric("r2_score", r2)
+    mlflow.log_metric("mse", mse)
+
+    # Log Model & Register
+    result = mlflow.sklearn.log_model(
+        sk_model=mind,
+        artifact_path="model"
+    )
+
+    mlflow.register_model(
+        model_uri=result.model_uri,
+        name="my_linear_model"
+    )
+
+    print(f"Model logged with r2: {r2}")
+
+with open("model.pkl", "wb") as file:
+    pickle.dump(mind, file)
+
 
 dump(mind, "AgeSalaryModel.pkl")
